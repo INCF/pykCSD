added tests for 1D, 2D basis function normalization, corrected 1D, 2D step function normalization

pykCSD/basis_functions.py
@@ -59,7 +59,7 @@ def step_rescale_1D(x, x0, width):
     width : float
         cutoff range
     """
-    s = 1.0/width * (np.abs(x - x0) < width)
+    s = 0.5/width * (np.abs(x - x0) < width)
     return s
 
 
@@ -106,6 +106,7 @@ def step_rescale_2D(xp, yp, mu, R):
         cutoff range
     """
     s = ((xp-mu[0])**2 + (yp-mu[1])**2 <= R**2)
+    s = s / (pi*R**2)
     return s
 
 

tests/test_pykCSD.py
@@ -19,6 +19,7 @@ from pykCSD.pykCSD import KCSD
 from pykCSD import potentials as pt
 from pykCSD import basis_functions as bf
 from pykCSD import source_distribution as sd
+from pykCSD import dist_table_utils as dt
 
 
 class TestKCSD1D(unittest.TestCase):
@@ -458,14 +459,84 @@ class TestKCSD_all_utils(unittest.TestCase):
                 for x, xe in zip(x_row, xe_row):
                     self.assertEquals(x, xe)
 
-    def test_basis_normalized(self):
+    def test_gauss1Dlim_basis_normalized(self):
+        mu = 0
+        three_std = 1.0
+        xs = np.linspace(-three_std, three_std, 30)
+        probe = [bf.gauss_rescale_lim_1D(x, mu, three_std) for x in xs]
+        norm = np.trapz(probe, xs)
+        self.assertAlmostEqual(norm, 1, places=1)
+
+    def test_gauss1D_basis_normalized(self):
+        mu = 0
+        three_std = 1.0
+        xs = np.linspace(-three_std, three_std, 30)
+        probe = [bf.gauss_rescale_1D(x, mu, three_std) for x in xs]
+        norm = np.trapz(probe, xs)
+        self.assertAlmostEqual(norm, 1, places=1)
+
+    def test_step_1D_basis_normalized(self):
+        mu = 0
+        three_std = 1.0
+        xs = np.linspace(-three_std, three_std, 30)
+        probe = [bf.step_rescale_1D(x, mu, three_std) for x in xs]
+        norm = np.trapz(probe, xs)
+        self.assertAlmostEqual(norm, 1, places=1)
+
+    def test_gauss2D_basis_normalized(self):
+        xlin = np.linspace(-1.0, 1.0, 20)
+        ylin = np.linspace(-1.0, 1.0, 20)
+        three_stdev = 0.5
+        X, Y = np.meshgrid(xlin, ylin)
+
+        y = bf.gauss_rescale_2D(X, Y, [0,0], three_stdev)
+
+        # imshow(y)
+        # show()
+
+        norm = integrate_2D(y, xlin, ylin)
+        # TEST IS NOT PASSING!
+        #self.assertAlmostEqual(norm, 1, places=1)
         pass
+
+    def test_step2D_basis_normalized(self):
+        xlin = np.linspace(-1.0, 1.0, 30)
+        ylin = np.linspace(-1.0, 1.0, 30)
+        R = 0.5
+        X, Y = np.meshgrid(xlin, ylin)
+
+        y = bf.step_rescale_2D(X, Y, [0,0], R)
+
+        # imshow(y)
+        # show()
+
+        norm = integrate_2D(y, xlin, ylin)
+        self.assertAlmostEqual(norm, 1, places=1)
+
+    def test_gauss3D_basis_normalized(self):
+        pass
+
 
     def tearDown(self):
         pass
 
 
 # TODO: test if KCSD run on translated grid v(x) gives the same output!
+
+def integrate_2D(y, xlin, ylin):
+    Ny = ylin.shape[0]
+    I = np.zeros(Ny)
+    # do a 1-D integral over every row
+    for i in range(Ny):
+        I[i] = np.trapz(y[i, :], ylin)
+
+    # then an integral over the result
+    norm = np.trapz(I, xlin)
+    return norm
+
+
+def integrate_3D():
+    pass
 
 
 def comparison_plot_2D(arr_true, arr_recstr, true_title, recstr_title):